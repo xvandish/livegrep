32fbb9f1f2452370913a1f1f7a1e28ed5785b296
Rodrigo Silva Mendoza <rodrigosilvamendoza@hotmail.com>
2022-01-13 22:06:57 -0700

    Add header to blame view, remove jquery dep for line highlighting

--- web/htdocs/assets/raw-js/blame.js
+++ web/htdocs/assets/raw-js/blame.js
@@ -1,50 +1,63 @@
 (function f() {
-    
+  const blameLinks = document.querySelectorAll('#blamefile #hashes a');
 
-  console.log("lets try this again");
-  var $body = $("body");
+  const matchingLinksCache = {}; // commitHash: NodeList
+  function getLinksWithSameCommitHash(target) {
+    const href = target.getAttribute('href') || "";
+    const i = href.indexOf('.');
+    if (i == -1) return { matchingLinks: [], charAfterDot: '' };
+    const commitHash = href.substring(0, i);
 
-  /* In the file view, highlight the contents of each diff whose
-       commit the user mouses over. */
+    let matchingLinks = matchingLinksCache[commitHash];
+    if (!matchingLinks) {
+      // fetch
+      matchingLinks = document.querySelectorAll(
+        '#blamefile #hashes a[href^="' + commitHash + '"]'
+      );
+      matchingLinksCache[commitHash] = matchingLinks;
+    }
+    return { matchingLinks, charAfterDot: href.substr(i+1, 1) };
+  }
 
-  if ($body.hasClass("blamefile")) {
-    $body.on("mouseenter", "#hashes > a", function (e) {
-      var href = $(e.target).attr("href") || "";
-      var i = href.indexOf(".");
-      if (i == -1) return;
-      var commitHash = href.substring(0, i);
-      var cls = "highlight " + href.substr(i + 1, 1);
-      $('#hashes a[href^="' + commitHash + '"]').addClass(cls);
-    });
-    $body.on("mouseleave", "#hashes > a", function (e) {
-      var href = $(e.target).attr("href") || "";
-      var i = href.indexOf(".");
-      if (i == -1) return;
-      var commitHash = href.substring(0, i);
-      var cls = "highlight " + href.substr(i + 1, 1);
-      $('#hashes a[href^="' + commitHash + '"]').removeClass(cls);
-    });
+  function highlightMatchingLines(event) {
+    const { matchingLinks, charAfterDot } = getLinksWithSameCommitHash(event.target);
+      console.log({ matchingLinks });
+    for (var idx = 0; idx < matchingLinks.length; idx++) {
+      matchingLinks[idx].classList.add('highlight', charAfterDot);
+    }
+  }
+
+  function removeHighlightFromMatchingLines(event) {
+    const { matchingLinks, charAfterDot } = getLinksWithSameCommitHash(event.target);
+    for (var idx = 0; idx < matchingLinks.length; idx++) {
+      matchingLinks[idx].classList.remove('highlight', charAfterDot);
+    }
+  }
+
+  for (var i = 0; i < blameLinks.length; i++) {
+    blameLinks[i].addEventListener('mouseenter', highlightMatchingLines);
+    blameLinks[i].addEventListener('mouseleave', removeHighlightFromMatchingLines);
   }
 
   /* When the user clicks a hash, remember the line's y coordinate,
-       and warp it back to its current location when we land. */
-       // TODO: this wasn't working anyways
+         and warp it back to its current location when we land. */
+  // TODO: this wasn't working anyways
 
-//   $body.on("click", "#hashes > a", function (e) {
-//     var y = $(e.currentTarget).offset().top - $(window).scrollTop();
-//     Cookies.set("pre", y, { expires: 1 });
-//     // (Then, let the click proceed with its usual effect.)
-//   });
+  //   $body.on("click", "#hashes > a", function (e) {
+  //     var y = $(e.currentTarget).offset().top - $(window).scrollTop();
+  //     Cookies.set("pre", y, { expires: 1 });
+  //     // (Then, let the click proceed with its usual effect.)
+  //   });
 
-//   var previous_y = Cookies.get("pre");
-//   if (typeof previous_y !== "undefined") {
-//     Cookies.remove("pre");
-//   }
+  //   var previous_y = Cookies.get("pre");
+  //   if (typeof previous_y !== "undefined") {
+  //     Cookies.remove("pre");
+  //   }
 
   // After the dom loads, check if we have a line # selected
   // if we do, attempt to scroll to it -
   // not sure how target
-  window.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener('DOMContentLoaded', function () {
     let lineNum = window.location.hash;
     if (!lineNum) return;
 
@@ -57,10 +70,10 @@
     // when I later refactor the blame links, I can use this function instead,
     // while taking into consideration the postion of a previously clicked blame
     // link (the one stored into cookies above)
-    // idk what to tell say - without this timeout, this line behaves 
+    // idk what to tell say - without this timeout, this line behaves
     // like default scrollIntoView(), e.g. scrollIntoView({ block: 'start' })
-    setTimeout(function() {
-        target.scrollIntoView({ block: 'center' })
-    }, 2)
+    setTimeout(function () {
+      target.scrollIntoView({ block: 'center' });
+    }, 2);
   });
 })();
