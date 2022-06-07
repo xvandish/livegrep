fca5da7ee23c22990c4d6f920594cde4b98159e1
Rodrigo Silva Mendoza <rodrigo.silvamendoza@nytimes.com>
2022-06-07 11:05:55 -0700

    use JS to remove 'Select All' button on repo selector

--- web/src/codesearch/repo_selector.js
+++ web/src/codesearch/repo_selector.js
@@ -24,6 +24,21 @@ function init() {
             $('.bootstrap-select .bs-searchbox input').focus();
         }
     });
+
+    // We don't want the "Select All" button as that results in an HTTP request
+    // that's too large. Additionally, the UI doesn't properly
+    // bubble up that error, leading to a confusing state. We could just check
+    // if "all repos" are selected, then just remove them since "all repos" are
+    // searched by default. But for now, we simply remove the "Select All"
+    // button to prevent the scenario entirely. The bootstrap-select library
+    // provides no native way to do this, so we do it the hard way
+    var replaceSelectAllInterval = setInterval(function () {
+        selectAllBtn = document.getElementsByClassName("actions-btn bs-select-all btn btn-default");
+        if (selectAllBtn.length > 0) {
+            selectAllBtn[0].remove();
+            clearInterval(replaceSelectAllInterval);
+        }
+    }, 10);
 }
 
 function updateOptions(newOptions) {
